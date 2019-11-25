9a4c48aceb20ec383a10d9bfbbb025cf10d2689f
jimmypun <jimmypun@nwconstruction.com.hk>
2019-11-25 12:24:59 +0800

    restructure

--- Plugin.XF.Controls.iOS/Renderer/EnhancedWebViewRenderer.cs
+++ Plugin.XF.Controls.iOS/Renderer/EnhancedWebViewRenderer.cs
@@ -13,7 +13,17 @@ using Xamarin.Forms.Platform.iOS;
 [assembly: ExportRenderer(typeof(EnhancedWebView), typeof(EnhancedWebViewRenderer))]
 namespace Plugin.XF.Controls.iOS.Renderer
 {
-
+    public partial class WebViewDelegate : WKNavigationDelegate, INSUrlConnectionDataDelegate
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public override void DidReceiveAuthenticationChallenge(WKWebView webView, NSUrlAuthenticationChallenge challenge, Action<NSUrlSessionAuthChallengeDisposition, NSUrlCredential> completionHandler)
+        {
+            //base.DidReceiveAuthenticationChallenge(webView, challenge, completionHandler);
+            completionHandler(NSUrlSessionAuthChallengeDisposition.UseCredential, new NSUrlCredential(Username, Password, NSUrlCredentialPersistence.ForSession));
+            return;
+        }
+    }
     public class EnhancedWebViewRenderer : ViewRenderer<EnhancedWebView, WKWebView>
     {
         public static void Init() { }
